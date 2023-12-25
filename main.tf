8793144381913c44d3904fdf077da5e5ffe88989
vikasvicky1801 <124026377+vikasvicky1801@users.noreply.github.com>
2023-12-25 12:30:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,4 @@
 
-resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr_block
-
-  tags = {
-    Name = "AD-VPC"
-  }
-}
-
-resource "aws_subnet" "subnet" {
-  count = length(var.subnet_cidr_blocks)
-
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr_blocks[count.index]
-  availability_zone       = var.availability_zone [count.index]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "AD-Subnet-${count.index + 1}"
-  }
-}
-
 resource "aws_directory_service_directory" "aws_directory" {
   name     = var.directory_name
   password = random_password.ad_password.result
@@ -28,8 +7,8 @@ resource "aws_directory_service_directory" "aws_directory" {
   type     = var.type
 
   vpc_settings {
-    vpc_id     = aws_vpc.vpc.id
-    subnet_ids = aws_subnet.subnet[*].id
+    vpc_id     = var.vpc_id
+    subnet_ids = var.subnet_id
   }
 }
 
